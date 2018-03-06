ecd48af67604aa32b20997ab544cb51d11ef6537
Byron <jamesblocker81@gmail.com>
2018-03-06 12:33:24 -0800

    build in throttle function

--- second-express/index.js
+++ second-express/index.js
@@ -4,8 +4,19 @@ const logger = require('morgan')
 const fs = require('fs')
 const PORT = 3000
 
-//log request to terminal
-app.use(logger('dev'))
+// log request to terminal
+function customLogger(req, res, next) {
+    console.log(req.url)
+    next()
+}
+
+// slowdown server intentionally
+function throttle(req, res, next) {
+    setTimeout(next, 5000)
+}
+
+app.use(customLogger)
+app.use(throttle)
 
 app.get('/', function(req, res) {
     res.send('Home page!')
@@ -16,10 +27,10 @@ app.get('/users', function(req, res) {
 })
 
 // params built in express :something
-app.get('/users/:userId/posts/:postId', function(req, res){
-    console.log(req.params)
-    res.send(`<h1>params example</h1>`)
-})
+// app.get('/users/:userId/posts/:postId', function(req, res){
+//     console.log(req.params)
+//     res.send(`<h1>params example</h1>`)
+// })
 
 app.listen(PORT, function(err) {
     console.log(err || `Server running on ${PORT}`)
