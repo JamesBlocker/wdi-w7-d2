ab1e8cdf9081cdf2ac4ed97d1ce914a4d2a574b1
Byron <jamesblocker81@gmail.com>
2018-03-06 12:43:32 -0800

    add about, contact, and users pages

--- tres-express/index.js
+++ tres-express/index.js
@@ -13,6 +13,14 @@ app.get('/contact', function(req, res) {
     res.send('<h1>Contact page MUTHA FUCKA!!!</h1>');
 })
 
+app.get('/about', function(req, res) {
+    res.send('<h1>About page MUTHA FUCKA!!!!</h1>')
+})
+
+app.get('/users', function(req, res) {
+    res.send('<h1>User page MUTHA FUCKA!!!!</h1>')
+})
+
 app.listen(PORT, function(err) {
     console.log(err || `Server is running on port ${PORT}`)
 })
