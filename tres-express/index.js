50b89fd6685a8f9a218956df8fe7e6a48bc260f5
Byron <jamesblocker81@gmail.com>
2018-03-06 12:40:28 -0800

    build third express framework for practice

---
+++ tres-express/index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const logger = require('morgan')
+const PORT = 3000
+
+app.use(logger('dev'))
+
+app.get('/', function(req, res) {
+    res.send('<h1>Home page mutha fucka!!!!</h1')
+})
+
+app.get('/contact', function(req, res) {
+    res.send('<h1>Contact page MUTHA FUCKA!!!</h1>');
+})
+
+app.listen(PORT, function(err) {
+    console.log(err || `Server is running on port ${PORT}`)
+})
